e1879e937d00fdcb9b6a8220b0beb344fb20f815
izakfr <izakfr@umich.edu>
2018-03-08 21:22:08 -0500

    Remove api_caller class and add functionality to bittrex_api wrapper class

--- data_processor.py
+++ data_processor.py
@@ -40,71 +40,17 @@ class db_manager():
         self.c.execute(db_command)
         self.conn.commit()
 
-    def get_line(self, table, field={}):
-        db_command = "SELECT * FROM " + table + " WHERE " + field[0] + "=(?)"
+    def get_line_for_param(self, table, field={}):
+        db_command = "SELECT * FROM " + table + " WHERE " + field[0] + "=" + field[1]
         self.c.execute(table_command, (field[1],))
+        return c.fetchone()
 
     def get_all(self, table):
         db_command = "SELECT * FROM " + table
         self.c.execute(db_command)
+        return c.fetchall()
 
     def get_last_n_rows(self, table, period):
         db_command = "SELECT price FROM " + table + " ORDER BY rowid DESC LIMIT " + str(period) + ";"
         self.c.execute(db_command)
-
-class api_calls():
-    def __init__(self):
-        # List of all possible commands
-        self.wrapper = wrapper.bittrex_wrapper()
-        command_list = ["getticker", "getmarketsummaries", "getmarketsummary",
-                        "getorderbook", "buylimit", "selllimit", "cancel",
-                        "getopenorders", "getbalances", "getbalance", "getorder"]
-
-    # Define functions for each command
-    def getticker(self, pair):
-        return self.wrapper.process_command("getticker", "public",
-                                            {'market': str(pair)})
-
-    def getmarketsummaries(self):
-        return self.wrapper.process_command("getmarketsummaries", "public")
-
-    def getmarketsummary(self, pair):
-        return self.wrapper.process_command("getmarketsummary", "public",
-                                            {'market': str(pair)})
-
-    # Booktype must be 'sell', 'buy', or 'both'
-    def getorderbook(self, pair, booktype):
-        return self.wrapper.process_command("getorderbook", "public",
-                                            {'market': str(pair),
-                                             'type': booktype})
-
-    def buylimit(self, pair, quantity, rate):
-        return self.wrapper.process_command("buylimit", "market",
-                                            {'market': str(pair),
-                                             'quantity': quantity,
-                                             'rate': rate})
-
-    def selllimit(self, pair, quantity, rate):
-        return self.wrapper.process_command("selllimit", "market",
-                                            {'market': str(pair),
-                                             'quantity': quantity,
-                                             'rate': rate})
-
-    def cancel(self, order_id):
-        return self.wrapper.process_command("cancel", "market",
-                                            {'uuid': order_id})
-
-    def getopenorders(self, pair):
-        return self.wrapper.process_command("getopenorders", "market",
-                                            {'market': str(pair)})
-
-    def getbalances(self):
-        return self.wrapper.process_command("getbalances", "account")
-
-    def getbalance(self, pair):
-        return self.wrapper.process_command("getbalance", "account",
-                                            {'currency': str(pair)})
-
-    def getorder(self, order_id):
-        return self.wrapper.process_command("getorder", "account",
-                                            {'uuid': order_id})
+        return c.fetchall()
--- wrapper.py
+++ wrapper.py
@@ -36,3 +36,55 @@ class bittrex_wrapper:
                            hashlib.sha512).hexdigest()
 
         return requests.get(request_url, headers={"apisign": apisign}).json()
+
+    # Define functions for each command
+    def getticker(self, pair):
+        return self.process_command("getticker", "public",
+                                    {'market': str(pair)})
+
+    def getmarketsummaries(self):
+        return self.process_command("getmarketsummaries", "public")
+
+    def getmarketsummary(self, pair):
+        return self.process_command("getmarketsummary", "public",
+                                    {'market': str(pair)})
+
+    # Booktype must be 'sell', 'buy', or 'both'
+    def getorderbook(self, pair, booktype):
+        return self.process_command("getorderbook", "public",
+                                    {'market': str(pair),
+                                     'type': booktype})
+
+    def buylimit(self, pair, quantity, rate):
+        return self.process_command("buylimit", "market",
+                                    {'market': str(pair),
+                                     'quantity': quantity,
+                                     'rate': rate})
+
+    def selllimit(self, pair, quantity, rate):
+        return self.process_command("selllimit", "market",
+                                    {'market': str(pair),
+                                     'quantity': quantity,
+                                     'rate': rate})
+
+    def cancel(self, order_id):
+        return self.process_command("cancel", "market",
+                                    {'uuid': order_id})
+
+    def getopenorders(self, pair):
+        return self.process_command("getopenorders", "market",
+                                    {'market': str(pair)})
+
+    def getorderhistory(self, pair):
+        return self.process_command("getorderhistory", "account")
+
+    def getbalances(self):
+        return self.process_command("getbalances", "account")
+
+    def getbalance(self, pair):
+        return self.process_command("getbalance", "account",
+                                    {'currency': str(pair)})
+
+    def getorder(self, order_id):
+        return self.process_command("getorder", "account",
+                                    {'uuid': order_id})
