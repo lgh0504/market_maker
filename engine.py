e5eff55a9c046580f19b92b08423c6d698bc37a0
izakfr <izakfr@umich.edu>
2018-03-09 19:17:21 -0500

    Add orderManger class to engine.py
    The order manager keeps track of all open and closed orders made by the bot

--- engine.py
+++ engine.py
@@ -12,31 +12,41 @@ import data_processor
 # Order Sizing
 # Mode of Market Making (Aggressive vs. Passive)
 # Reporting
-class trading_engine():
+class tradingEngine():
     def __init__(self):
-        self.openOrders = order_database()
-        self.closedOrders = order_database()
+        self.orderManager = orderManager()
 
 # The order database will keep track of all orders and will store them in a table
 # The first table will contain a list of all of the orders made, and store them
 # in the following columns: uuid | price | size | filled? | date | time
-class order_database():
+class orderManager():
     def __init__(self):
-        # Create a new database
-        self.orderDb = data_processor.db_manager("orderDb")
-        self.orderDb.create_table("orders",
+        # Create a databse with two tables to manage open orders and closed orders
+        self.orderDb = data_processor.db_manager("openOrderDb")
+        self.orderDb.create_table("open",
+                                  ['uuid', 'price', 'size', 'filled', 'date', 'time'],
+                                  ['TEXT', 'REAL', 'REAL', 'INTEGER', 'TEXT', 'TEXT'])
+        self.orderDb.create_table("closed",
                                   ['uuid', 'price', 'size', 'filled', 'date', 'time'],
                                   ['TEXT', 'REAL', 'REAL', 'INTEGER', 'TEXT', 'TEXT'])
 
     # Get an order by ID
-    def get_order_by_ID(self, uuid):
-        return self.orderDb.get_line_for_param("orderDb", ['uuid', uuid]))
+    def get_order_by_ID(self, uuid, open):
+        if (open == 1):
+            return self.openOrderDb.get_line_for_param("open", ['uuid', uuid]))
+        else:
+            return self.closedOrderDb.get_line_for_param("closed", ['uuid', uuid]))
 
     # Insert an order into the db
-    def insert_order(self, uuid, price, size, filled, date, time):
-        self.orderDb.insert_entry("orderDb", [uuid, price, size, filled, date, time])
+    def insert_order_to_db(self, uuid, price, size, filled, date, time, open):
+        if (open == 1):
+            self.orderDb.insert_entry("open", [uuid, price, size, filled, date, time])
+        else:
+            self.orderDb.insert_entry("closed", [uuid, price, size, filled, date, time])
 
     # Delete an order by ID
-    def delete_order(self, uuid):
-        self.orderDb.get_line_for_param("orderDb", ['uuid', uuid])
-        
+    def delete_order(self, uuid, open):
+        if (open == 1):
+            self.orderDb.get_line_for_param("open", ['uuid', uuid])
+        else:
+            self.orderDb.get_line_for_param("closed", ['uuid', uuid])
